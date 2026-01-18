3626585103c75bd95a0f0e5ecc223dc4cdfaecb2
CHCAT1320 <108920403+CHCAT1320@users.noreply.github.com>
2026-01-18 22:14:14 +0800

    Update getUser.py

--- getUser.py
+++ getUser.py
@@ -2,6 +2,7 @@ import requests
 import asyncio
 import gameDataAes2Json
 import base64
+import json
 
 class user:
     def __init__(self, phone):
@@ -12,7 +13,7 @@ class user:
         if type == "GET":
             response = requests.get(url, headers=headers, verify=False)
         elif type == "POST":
-            response = requests.post(url, headers=headers, data=data, verify=False)
+            response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
         # 打印响应内容
         print(f"\n响应内容：{response.text}")
         try:
@@ -46,25 +47,33 @@ class user:
             "transaction": "login"
         }, "POST")
 
-    async def login(self, verifyCode):
+    async def login(self, password):
         # 目标请求链接
         url = "https://rizserver.pigeongames.net/account/login"
 
         # 你的完整请求头（原样复制，全部保留，这是请求成功的核心）
         headers = {
-        "Host": "rizserver.pigeongames.net",
+        # "Host": "rizserver.pigeongames.net",
+        "User-Agent": "UnityPlayer/2022.3.62f2 (UnityWebRequest/1.0, libcurl/8.10.1-DEV)",
         "game_id": "pigeongames.rizline",
-        "device_id": "f18c2696-461d-43a2-a214-f27fb85811a1",
+        "device_id": "0224fa25-254f-49d0-84b7-0df4f5de63b6",
         "channel_id": "11",
         "i18n": "zh-CN",
         "Content-Type": "application/json",
         "phone": self.phone,
         }
-
-        result = await self.request(url, headers, {
-            "phone": self.phone,
-            "code": verifyCode
-        }, "POST")
+        if password == "":
+            await self.sendVerifyCode()
+            verifyCode = input("请输入验证码：")
+            result = await self.request(url, headers, {
+                "phone": self.phone,
+                "code": verifyCode
+            }, "POST")
+        else:
+            result = await self.request(url, headers, {
+                "phone": self.phone,
+                "password": password
+            }, "POST")
         for k, v in result.headers.items():
             if k == "set_token":
                 print(f"\n登录成功，token为：{v}")
@@ -75,6 +84,7 @@ class user:
         url = "https://rizserver.pigeongames.net/game/rn_login"
         headers = {
         "Host": "rizserver.pigeongames.net",
+        "User-Agent": "UnityPlayer/2022.3.62f2 (UnityWebRequest/1.0, libcurl/8.10.1-DEV)",
         "game_id": "pigeongames.rizline",
         "device_id": "f18c2696-461d-43a2-a214-f27fb85811a1",
         "channel_id": "11",
@@ -93,13 +103,14 @@ class user:
         print(result.text)
         resultGameData = gameDataAes2Json.rizline_aes_decrypt(base64.b64decode(result.text))
         print("\n解密后的游戏数据：" + resultGameData)
+        with open("gameData.json", "w", encoding="utf-8") as f:
+            f.write(resultGameData)
 
 if __name__ == '__main__':
-    phone = ""
+    phone = input("请输入手机号：")
     userI = user(phone)
-    asyncio.run(userI.sendVerifyCode())
-    verifyCode = input("请输入验证码：")
-    asyncio.run(userI.login(verifyCode))
+    password = input("请输入密码：")
+    asyncio.run(userI.login(password))
     asyncio.run(userI.getUserInfo())
 
 
